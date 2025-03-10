0fa8344b8bb8ad2f672f5836f3c58542627ea1fd
Atchutha Rao T A <atchutha57@gmail.com>
2025-03-10 16:45:38 +0530

    Updated reminderdb within remindtes.ts

--- src/reminders.ts
+++ src/reminders.ts
@@ -1,46 +1,44 @@
-class Reminder {
-    id: string;
+class reminder{
+    id: number;
     title: string;
-    description: string;
     date: Date;
-    constructor(id: string, title: string, description: string, date: Date) {
+    constructor(id: number, title: string, date: Date){
         this.id = id;
         this.title = title;
-        this.description = description;
         this.date = date;
     }
 }
-class ReminderDatabase {
-    reminders: Reminder[] = [];
-    createReminder(id: string, title: string, description: string, date: Date): void {
-        this.reminders.push(new Reminder(id, title, description, date));
+
+class ReminderDataBase{
+    reminders: Map<String, reminder> = new Map();
+    createReminder(title: string, date: Date){
+        const id = Math.floor(Math.random() * 1000);
+        const rem = new reminder(id, title, date);
+        this.reminders.set(id.toString(), rem);
     }
-    getAllReminders(): Reminder[] {
-        return this.reminders;
+    getAllReminders(): reminder[] | null{
+        return Array.from(this.reminders.values());
     }
-    getReminder(id: string): Reminder | null {
-        console.log("reminder found");
-        return this.reminders.find(reminder => reminder.id === id) || null;
+    getReminder(id: string): reminder | null{
+        return this.reminders.get(id) || null;
     }
-    removeReminder(id: string): void {
-        this.reminders = this.reminders.filter(reminder => reminder.id !== id);
+
+    removeReminder(id: string){
+        this.reminders.delete(id);
     }
-    exists(id: string): string | null {
-         if(this.reminders.some(reminder => reminder.id === id)){
-            return "Reminder exists";
-         }
-         else{
-            return "Reminder does not exist";
-         }
+    updateReminder(id: string, title: string, date: Date){ 
+        const rem = this.reminders.get(id);
+        if(rem){
+            if(title){
+                rem.title = title;
+            }
+            if(date){
+                rem.date = date;
+            }
+            this.reminders.set(id, rem);
+
+        }   
     }
 }
-// Example usage:
-const reminderDB = new ReminderDatabase();
-reminderDB.createReminder("1", "Meeting", "Team meeting at 10 AM", new Date("2025-03-11T10:00:00"));
-reminderDB.createReminder("2", "Appointment with the doctor", "Meet dr.varun tomorrow", new Date("2025-03-11T13:00:00"));
-console.log(reminderDB.getAllReminders());
 
-console.log(reminderDB.getReminder("1"));
-reminderDB.removeReminder("1");
-console.log(reminderDB.getAllReminders());
-console.log(reminderDB.exists("1"));
+export default ReminderDataBase;
